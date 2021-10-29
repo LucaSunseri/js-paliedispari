4efb85df0a7ffe701d3821d8e7a56a6c0e744ea7
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-10-29 15:25:30 +0200

    end Pari e Dispari

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -7,18 +7,71 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-const userEvendOdd = 'pari'; //inserire prompt
-const userNumber = 5; //Inserire promt 
-console.log('Numero scelto utente:', userNumber);
+
+// Facciamo scegliere all'utente pari o dispari
+let userEvendOdd = prompt('Scegli pari o dispari');
+
+// Creiamo un ciclo in modo tale che fino a quando l'utente non inserisce in modo corretto pari o dispari non può andare avanti!
+while ((userEvendOdd !== 'pari') && (userEvendOdd !== 'dispari')) {
+  userEvendOdd = prompt('Attenzione scrivere pari o dispari in modo corretto!');
+}
+// console.log('Utente ha scelto:', userEvendOdd);
+
+
+// Facciamo scegliere all'utente un numero tra 1 e 5
+let userNumber = parseInt(prompt('Scegli un numero tra 1 e 5'));
+
+// Creiamo un ciclo in modo tale che fino a quando l'utente non inserisce un numero tra 1 e 5 non può andare avanti!
+while ((isNaN(userNumber)) || (userNumber <= 0) || (userNumber > 5)) {
+  userNumber = parseInt(prompt('Attenzione inserire un numero corretto tra 1 a 5!'));
+}
+// console.log('Numero scelto utente:', userNumber);
+
+
+// Estraiamo un numero casuale per il tra 1 e 5
 const pcNumber = randomNumber(1, 5);
-console.log('Numero casuale pc:', pcNumber);
+// console.log('Numero casuale pc:', pcNumber);
 
+
+// Facciamo la somma tra i 2 numeri
 const sumNumber = userNumber + pcNumber;
-console.log('Somma tra numero utente e pc', sumNumber);
+// console.log('Somma tra numero utente e pc', sumNumber);
 
-const win = evenOdd(sumNumber);
-console.log('Il vincitore è:', win);
 
+// Stabilia se il vincitore sarà pari o dispari
+const result = evenOdd(sumNumber);
+// console.log('Il vincitore è:', win);
+
+let win = '';
+
+// Decretiamo il vincitore
+if (result === userEvendOdd) {
+  win = `
+  <p>
+    Complimenti hai vinto
+  </p>  
+  `;
+  console.log('Utente è il vincitore');
+} else {
+  win = `
+  <p>
+    Il pc ha vinto
+  </p>  
+  `;
+  console.log('Pc è il vincitore');
+}
+
+// Inseriamo il utto nell HTML 
+const output = document.getElementById('gioco');
+
+output.innerHTML = `
+<p>
+  Hai scelto ${userEvendOdd} <br>
+  Il numero da te scelto è ${userNumber} e quello del pc è ${pcNumber} <br>
+  La somma dei due numeri è ${sumNumber} quindi è ${result} <br>
+  ${win}
+</p>  
+`;
 
 
 // Funzione che genera se un numero è pari o dispari
