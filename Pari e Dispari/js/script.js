085aaea2a0e4e3c664f9798a5757daf14657ba97
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-10-29 14:04:06 +0200

    Create two functions

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -8,10 +8,32 @@ Dichiariamo chi ha vinto.
 */
 
 const userEvendOdd = 'pari'; //inserire prompt
-const userNumber = 2; //Inserire promt 
+const userNumber = 5; //Inserire promt 
+console.log('Numero scelto utente:', userNumber);
 const pcNumber = randomNumber(1, 5);
-console.log(pcNumber);
+console.log('Numero casuale pc:', pcNumber);
 
+const sumNumber = userNumber + pcNumber;
+console.log('Somma tra numero utente e pc', sumNumber);
+
+const win = evenOdd(sumNumber);
+console.log('Il vincitore è:', win);
+
+
+
+// Funzione che genera se un numero è pari o dispari
+function evenOdd (numero) {
+
+  let even = true;
+  let result = 'pari';
+
+  if (numero % 2 !== 0) {
+    even = false;
+    result = 'dispari';
+  } 
+
+  return result;
+}
 
 // Funzione che genera un numero random da X a Y compresi
 function randomNumber(from, to) {
